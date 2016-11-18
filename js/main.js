70d7fd260f33082361500ebaaf9861fa4d7c11a0
webdevcanuck <cutone.sergio@gmail.com>
2016-11-18 17:26:56 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 
+(function ($, window, document) {
+
+    $(document).on("click", "#my_addToSelectedCities", function () {
+        var addList = [];
+        $('select[name=my_cityOptions] :selected').each(function (i, selected) {
+            $(this).attr('disabled','disabled');
+            addList[i] = '<option>' + $(selected).text() + "</option>";
+        });
+        $("#selectedcities").append(addList);
+    });
+
+    $(document).on("click", "#my_removeSelectedCities", function () {
+        var removeList = [];
+        $('select[name=my_selectedCities] :selected').each(function (i, selected) {
+            $(this).remove();
+        });
+    });
+
+})(window.jQuery, window, document);
